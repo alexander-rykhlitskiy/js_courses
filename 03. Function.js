26605ebbe1515a217a18852f2fea03a15ca908cc
Alexander Ryhlitsky <alexander.rykhlitskiy@gmail.com>
2020-07-23 18:13:13 +0300

    Update lesson 3

--- 03. Function.js
+++ 03. Function.js
@@ -1,4 +1,56 @@
-// ПРИМЕР 5
+// ЗАДАНИЕ-ПОВТОРЕНИЕ
+// Задать массив чисел.
+// Найти максимальное число в массиве
+// Найти минимальное число в массиве.
+// Вывести их в консоль
+
+let numbers = [2, 4, 1, 3, 29, 7];
+
+let minNumber = numbers[0], maxNumber = numbers[0];
+for (number of numbers) {
+  if (number < minNumber) {
+    minNumber = number;
+  }
+  if (number > maxNumber) {
+    maxNumber = number;
+  }
+}
+console.log(minNumber);
+console.log(maxNumber);
+
+// Тернарный оператор
+// https://learn.javascript.ru/ifelse
+let male = true;
+let gender = male ? 'man' : 'woman';
+
+// Тернарный оператор выше делает то же самое, что и следующий if,
+// но запись получается более короткой и наглядной
+let male = true;
+let gender;
+if (male) {
+    gender = 'man';
+} else{
+    gender = 'woman'
+}
+
+// ЗАДАНИЕ
+// Задать массив логических значений (true/false). Каждое значение обозначает мужчина (true) или женщина (false).
+// Из этого массива сформировать массив объектов такой же длины, как и заданный массив.
+// У каждого объекта должно быть одно поле, gender, которое равно man, если значение в заданном массиве - true, и woman, если значение в заданном массиве - false.
+// Т.е. из
+[true, false]
+// должно получиться
+[{ gender: 'man' }, { gender: 'woman' }]
+
+let males = [true, false, false, true];
+let result = [];
+for (let male of males) {
+  result[result.length] = { gender: male ? 'man' : 'woman' };
+}
+console.log(result);
+
+// Что если нам нужно создать не 1 массив, а 10?
+
 // Описание простых функций.
 // ЧТО НУЖНО ЗНАТЬ
 // Мы уже знаем, что prompt и alert - это функции.
@@ -6,18 +58,57 @@
 
 // Мы можем объявить свою функцию.
 // Функции в  JavaScript объявляются при помощи ключевого слова function.
+
 // Данная функция принимает один параметр, который называется name.
+function showGreeting(name) {
+  console.log("привет, " + name);
+}
+showGreeting("Misha");
+
+// ЗАДАНИЕ
+// Написать и вызвать функцию, которая принимает число и выводит в консоль его квадрат.
+function sqr(number) {
+  console.log(number * number);
+}
+
+// Данная функция возвращает число Пи
+function getPi() {
+  return 22 / 7;
+}
+
+// ЗАДАНИЕ
+// Написать и вызвать функцию, которая возвращает слово "Привет"
+function getGreeting() {
+  return "Привет";
+}
+
+// ЗАДАНИЕ
+// Написать и вызвать функцию, которая принимает один параметр, который называется name.
 // Далее она прибавляет name к "Привет, " и возвращает результат
 function myFunction(name) {
   return "Привет, " + name;
 }
-myFunction("Саша"); // = "Привет, Саша"
+myFunction("Миша"); // = "Привет, Миша"
 
 // Область видимости переменных.
 name = "banana"; // объявление глобальной переменной
-let name = "banana"; // объявление локальной переменной с блочной видимостью (напр. видна только в блоке if)
+let name = "banana"; // объявление локальной переменной с блочной видимостью (видна только в блоке кода (if, for, while))
 var name = "banana"; // объявление локальной переменной с функциональной видимостью (видна только в данной функции)
 
+// У переменной price в каждой функции и в блоке `if (true) {` свое значение,
+// которое не пересекается с другими переменными price
+function calcPrice(kilometers, minutes) {
+  var price = 2;
+  price = price + kilometers * 0.3;
+  price = price + minutes * 0.1;
+  if (true) {
+    let price = 5;
+  }
+  return price;
+}
+
+// ЗАДАНИЕ
+// Написать функцию, которая принимает массив чисел и возвращает максимальное число из массива.
 
 // Проитерироваться по полям объекта при помощи цикла for ... in
 for (let property in person) {
@@ -54,9 +145,6 @@ switch (color) {
 }
 // > Ехай
 
-// Тернарный оператор
-let a = true ? 3 : 5;
-
 // Вспомнить функции и массивы
 
 // ЗАДАНИЕ:
@@ -73,25 +161,6 @@ function clone(array) {
   return result;
 }
 clone([1,2,3]);
-
-// ЗАДАНИЕ:
-// Задать массив чисел. Найти максимальное число в массиве и минимальное. Вывести их в консоль
-
-// ЧТО НОВОГО:
-// Ключевое слово of.
-let numbers = [2, 4, 1, 3, 29, 7];
-
-let minNumber = numbers[0], maxNumber = numbers[0];
-for (number of numbers) {
-  if (number < minNumber) {
-    minNumber = number;
-  }
-  if (number > maxNumber) {
-    maxNumber = number;
-  }
-}
-console.log(minNumber);
-console.log(maxNumber);
 
 // ЗАДАНИЕ:
 // Напишите функцию range(start, end, step), которая возвращает массив всех чисел от start до end включительно с шагом step.
@@ -244,18 +313,6 @@ function pluck(array, key) {
 // функция concat
 
 
-// У переменной price в каждой функции и в блоке `if (true) {` свое значение,
-// которое не пересекается с другими переменными price
-function calcPrice(kilometers, minutes) {
-  var price = 2;
-  price = price + kilometers * 0.3;
-  price = price + minutes * 0.1;
-  if (true) {
-    let price = 5;
-  }
-  return price;
-}
-
 function calcDebit() {
   var price = 9;
   price = price * 2;
@@ -330,20 +387,7 @@ contains([1,2,3], 2)
 contains([1,2,3], 5)
 
 
-// Тернарный оператор
-// https://learn.javascript.ru/ifelse
-let male = true;
-let gender = male ? 'man' : 'woman';
 
-// Тернарный оператор выше делает то же самое, что и следующий if,
-// но запись получается более короткой и наглядной
-let male = true;
-let gender;
-if (male) {
-    gender = 'man';
-} else{
-    gender = 'woman'
-}
 
 
 // ПОДРОБНЕЕ ПРО ФУНКЦИИ
